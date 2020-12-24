f026b7554a082bce6e722031e56a552f248aab0c
RichardRivaldo <richardrivaldo84@gmail.com>
2020-12-25 00:30:39 +0700

    CatBoost

---
+++ Model Selection/CatBoost/CatBoost.py
@@ -0,0 +1,44 @@
+#%%
+# CatBoost
+
+#%%
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#%%
+# Dataset
+dataset = pd.read_csv("Data.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+#%%
+# Splitting the Dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#%%
+# CatBoost
+from catboost import CatBoostClassifier
+classifier = CatBoostClassifier()
+classifier.fit(X_train, y_train)
+
+#%%
+# Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+#%%
+# Accuracy
+Accuracy = accuracy_score(y_test, y_pred)
+print(Accuracy)
+
+#%%
+# K-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
