f34f530ea451691e72841345c829688170ea38b3
RichardRivaldo <richardrivaldo84@gmail.com>
2020-12-25 00:06:03 +0700

    Grid Search

--- Model Selection/K-Fold Cross Validation/K_Fold_Cross_Validation.py
+++ Model Selection/K-Fold Cross Validation/K_Fold_Cross_Validation.py
@@ -1,6 +1,6 @@
 #%%
 # K-Fold Cross Validation
-# There might be a data leakage happening in this code
+# There is a leakage happening in this code
 
 #%%
 # Libraries
@@ -27,7 +27,7 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 #%%
-# Training the Kernel SVM model on the Training set
+# Kernel SVM Model
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
@@ -50,6 +50,22 @@ from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
 print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
+#%%
+# Grid Search
+from sklearn.model_selection import GridSearchCV
+parameters = [{'C': [0.25, 0.5, 0.75, 1], 'kernel': ['linear']},
+              {'C': [0.25, 0.5, 0.75, 1], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10,
+                           n_jobs = -1)
+grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+print("Best Accuracy: {:.2f} %".format(best_accuracy*100))
+print("Best Parameters:", best_parameters)
 
 #%%
 # Training Set Visualization
